521d9d2924bfb86b3c1e62774e4342d447cf7df5
Chamanjeet-Singh <chamanjeet111@gmail.com>
2025-10-08 10:12:21 -0400

    Implement Navbar , Left and RightBar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,38 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = {firstName: 'ChamanJeet' , lastName: 'Singh'};
   return (
-    <div>
-      Home
-    </div>
+      <section className="home">
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.firstName || 'Guest'}
+                subtext="Access and manage your banking transaction efficiently."
+                
+                />
+                <TotalBalanceBox
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={1250.35}
+
+                />
+            </header>
+
+            RECENT TRANSACTIONS
+
+        </div>
+        <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[]}/>
+      </section>
+    
   )
 }
 
