769b13427cbf7d6e8a1674b66521be1d8d939455
Chamanjeet-Singh <chamanjeet111@gmail.com>
2025-10-09 19:35:47 -0400

    Implement and complete Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,7 @@ import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
-    const loggedIn = {firstName: 'ChamanJeet' , lastName: 'Singh'};
+    const loggedIn = {firstName: 'ChamanJeet' , lastName: 'Singh' , email: "chamanjeet111@gmail.com"};
   return (
       <section className="home">
         <div className="home-content">
@@ -30,7 +30,9 @@ const Home = () => {
         <RightSidebar 
         user={loggedIn}
         transactions={[]}
-        banks={[]}/>
+        banks={[{ currentBalance: 157.50},{
+          currentBalance: 187.50
+        }]}/>
       </section>
     
   )
