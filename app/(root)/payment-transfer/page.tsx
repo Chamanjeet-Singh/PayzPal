c6d05d9d1ec71d2d72f963655f62fc17c30d5ad4
Chamanjeet-Singh <chamanjeet111@gmail.com>
2025-11-25 08:40:00 -0500

    fix err

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,5 +1,4 @@
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
+
 
 import HeaderBox from '@/components/HeaderBox'
 import PaymentTransferForm from '@/components/PaymentTransferForm'
